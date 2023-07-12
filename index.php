8b3057f3db59c27bc49fa505f93ea5362cee834b
Ariful Hasan <arifhasan.mbstu@gmail.com>
2023-07-12 20:42:42 +0200

    update index file

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
 use Src\Controllers\CheckoutController;
-use Src\Services\Discount;
-use Src\Services\PricePolicy;
+use Src\Services\DiscountService;
+use Src\Services\PricePolicyService;
 
 require_once __DIR__ . '/src/bootstrap.php';
 
 $rules = [
-    'A' => new PricePolicy(50, new Discount(20, 3)),
-    'B' => new PricePolicy(30, new Discount(15, 2)),
-    'C' => new PricePolicy(20, null),
-    'D' => new PricePolicy(15, null),
+    'A' => new PricePolicyService(50, new DiscountService(20, 3)),
+    'B' => new PricePolicyService(30, new DiscountService(15, 2)),
+    'C' => new PricePolicyService(20, null),
+    'D' => new PricePolicyService(15, null),
 ];
 
 $checkout = new CheckoutController($rules);
